2a304366d7af0c02308195612e8156c6957139e0
Vadim <uvolkov@374@gmail.com>
2022-08-11 11:59:00 +0300

    Add task54

---
+++ TASK54/Program.cs
@@ -0,0 +1,60 @@
+﻿int m = InputInt("Please, enter the count of lines: ");
+int n = InputInt("Please, enter the count of columns: ");
+int[,] array = new int[m,n];
+
+FillArray(array);
+PrintArray(array);
+DescendingSort(array);
+PrintArray(array);
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void DescendingSort(int[,] arr)
+{
+    for (int m = 0; m < arr.GetLength(0); m++)
+    {
+        for (int i = 0; i < arr.GetLength(1); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1) - 1; j++)
+            {
+
+                if (arr[m, j] < arr[m, j + 1])
+                {
+                    int temp = arr[m, j];
+                    arr[m, j] = arr[m, j + 1];
+                    arr[m, j + 1] = temp;
+
+                }
+
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
